dbe64b422ef66e528bc1e49001155bcf0ba133f9
İhsan Alp Pekel <87542668+alppekel@users.noreply.github.com>
2023-02-06 14:11:49 +0300

    Create kali.sh

---
+++ kali.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update -y
+
+sudo apt install kali-tools-top10 -y
+
+
+###################### setting up rdp with xfce ######################
+
+sudo apt install kali-desktop-xfce xorg xrdp -y
+
+# configuring xrdp to listen to port 3390
+
+sudo sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
+
+# starting xrdp service
+
+sudo systemctl start xrdp
+sudo systemctl enable xrdp
+
+######################################################################
+
+echo "*************** done ***************"
